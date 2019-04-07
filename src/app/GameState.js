1767e13584180bb404ad66c7c030f36a366b6850
michael <michael.clark1992@gmail.com>
2019-04-06 20:26:46 -0500

    fixes

--- src/app/GameState.js
+++ src/app/GameState.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
 export const Game = {
-<<<<<<< HEAD
   board: {},
   score: 0,
   setBoard: () => {},
   addToScore: () => {}
-}
-=======
-  board: [],
-  setBoard: () => {}
 };
->>>>>>> eslint
 
 export const GameState = React.createContext(Game);
